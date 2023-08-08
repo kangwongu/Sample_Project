0d6439891f7e5b2fe1849fe331c7775a7d456c0f
kangwongu <kwg0527@naver.com>
2023-08-08 10:58:37 +0900

    initial commit

---
+++ src/test/java/com/example/samplesns/SampleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
