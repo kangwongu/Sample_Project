013709c34989e8e614ab54af44485cd62c8447ca
kangwongu <kwg0527@naver.com>
2023-08-23 10:43:05 +0900

    Feat[#3]: 필드 추가

--- src/main/java/com/example/samplesns/member/infrastructure/MemberEntity.java
+++ src/main/java/com/example/samplesns/member/infrastructure/MemberEntity.java
@@ -2,6 +2,7 @@ package com.example.samplesns.member.infrastructure;
 
 import com.example.samplesns.common.Timestamp;
 import com.example.samplesns.member.domain.Member;
+import com.example.samplesns.member.domain.MemberStatus;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -19,15 +20,34 @@ public class MemberEntity extends Timestamp {
     private Long id;
 
     private String email;
+    private String password;
     private String nickname;
     private LocalDate birthday;
+    private String certificationCode;
+    private MemberStatus status;
+
+    public static MemberEntity fromModel(Member member) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.id = member.getId();
+        memberEntity.email = member.getEmail();
+        memberEntity.password = member.getPassword();
+        memberEntity.nickname = member.getNickname();
+        memberEntity.birthday = member.getBirthday();
+        memberEntity.certificationCode = member.getCertificationCode();
+        memberEntity.status = member.getStatus();
+
+        return memberEntity;
+    }
 
     public Member toModel() {
         return Member.builder()
                 .id(id)
                 .email(email)
+                .password(password)
                 .nickname(nickname)
                 .birthday(birthday)
+                .certificationCode(certificationCode)
+                .status(status)
                 .createDate(getCreateDate())
                 .modifyDate(getModifyDate())
                 .build();
