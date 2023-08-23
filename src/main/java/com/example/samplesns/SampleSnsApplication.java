9ebe10e52d9b72c2e7a73ac613615435ab38e195
kangwongu <kwg0527@naver.com>
2023-08-23 11:36:56 +0900

    Feat[#3]: Update MemberStatus Enum

--- src/main/java/com/example/samplesns/SampleSnsApplication.java
+++ src/main/java/com/example/samplesns/SampleSnsApplication.java
@@ -2,8 +2,10 @@ package com.example.samplesns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SampleSnsApplication {
 
     public static void main(String[] args) {
