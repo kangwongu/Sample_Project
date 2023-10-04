6721bfcad43785fd94dc919593b3476c9defceb8
kangwongu <kwg0527@naver.com>
2023-10-04 15:26:30 +0900

    Docs[#27]: Create 특정 회원이 작성한 게시글 목록 조회 스웨거

--- src/main/java/com/example/samplesns/post/controller/PostController.java
+++ src/main/java/com/example/samplesns/post/controller/PostController.java
@@ -8,6 +8,7 @@ import com.example.samplesns.post.dto.PostCreateRequest;
 import com.example.samplesns.post.dto.PostResponse;
 import com.example.samplesns.post.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -74,7 +75,14 @@ public class PostController {
     }
 
     @GetMapping("/list")
-    public ResponseEntity<Slice<PostResponse>> getPosts(@RequestParam String email,
+    @Operation(summary = "다른 회원이 작성한 게시글 목록 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = {@Content(array = @ArraySchema(schema = @Schema(implementation = PostResponse.class)))}),
+            @ApiResponse(responseCode = "404", description = "해당 회원이 존재하지 않음",
+                    content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
+    public ResponseEntity<Slice<PostResponse>> getPosts(@RequestParam @Parameter(description = "조회하려는 회원의 이메일", required = true) String email,
                                                         Pageable pageable) {
         Slice<PostResponse> response = postService.getPosts(email, pageable);
 
